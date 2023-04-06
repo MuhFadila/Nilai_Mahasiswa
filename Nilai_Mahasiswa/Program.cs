58a2cef2580f36516d0666e589574b098b799591
Muhammad Fadila <muh.fadila18@gmail.com>
2023-04-06 09:40:46 +0700

    Add project files.

---
+++ Nilai_Mahasiswa/Program.cs
@@ -0,0 +1,54 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nilai_Mahasiswa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Koneksi ke Database\n");
+                    Console.WriteLine("Masukan Server :");
+                    string server = Console.ReadLine();
+                    Console.WriteLine("Masukan User ID : ");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukan Password : ");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("Masukan database tujuan : ");
+                    string db = Console.ReadLine();
+                    Console.Write("\nKetik K untuk Terhubung ke Database");
+                    char chr = Convert.ToChar(Console.ReadLine());
+                    switch (chr)
+                    {
+                        case 'K':
+                            {
+                                MySqlConnection conn;
+                                string connectionString;
+                                connectionString = "SERVER=" + server + ";DATABASE=" + db + ";UserID=" + user + ";PASSWORD=" + pass + ";Port=3306";
+
+                                conn = new MySqlConnection(connectionString);
+                                conn.Open();
+                                Console.Clear();
+                                while (true)
+                                {
+
+                                }
+                            }
+                    }
+               
+                }
+                catch { }
+            }
+        }
+    }
+}
